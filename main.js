c462fc5a69347bd7f5c4b33af5300afb9346dc95
Mark Colby <macolby14@gmail.com>
2024-10-07 21:59:28 -0400

    Add url reload trying

--- main.js
+++ main.js
@@ -4,22 +4,57 @@ import electronUpdater from "electron-updater";
 
 const { autoUpdater } = electronUpdater;
 
-const createWindow = () => {
+const UV_INDEX_URL = "https://uv.markcolby.dev";
+const RETRY_INTERVAL_SEC = 5;
+
+function isDev() {
+  return process.env.NODE_ENV === "dev";
+}
+
+async function loadUrl(win, url) {
+  return new Promise((resolve, reject) => {
+    win
+      .loadURL(url)
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+const createWindow = async () => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
       preload: path.join(import.meta.dirname, "preload.js"),
     },
-    fullscreen: true,
+    fullscreen: !isDev(),
   });
 
-  mainWindow.maximize(); // incase full screen mode exited
+  if (!isDev()) {
+    mainWindow.maximize(); // incase full screen mode exited
+  } else {
+    mainWindow.width = 800;
+    mainWindow.height = 400;
+  }
 
-  const uvIndexUrl =
-    process.env.NODE_ENV === "dev"
-      ? "http://localhost:5173"
-      : "https://uv.markcolby.dev";
+  const loadedSuccessfully = false;
 
-  mainWindow.loadURL(uvIndexUrl);
+  // try to load the page every second until it loads
+  do {
+    await loadUrl(mainWindow, UV_INDEX_URL)
+      .then(() => {
+        loadedSuccessfully = true;
+      })
+      .catch((err) => {
+        console.error("Failed to load URL", err);
+      });
+    await new Promise((resolve) =>
+      setTimeout(resolve, RETRY_INTERVAL_SEC * 1000)
+    );
+  } while (!loadedSuccessfully);
+
   autoUpdater.setFeedURL({
     provider: "github",
     owner: "macolby14",
